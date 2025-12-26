919771c8831884b42bf66657e18eef42064b17fe
ndeskaj <n.deskaj@gmx.de>
2025-12-26 21:57:17 +0100

    set up scanner

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Worm loaded")
+
+	target := "127.0.0.1"
+	fmt.Printf("[*] Starting active scan on %s...\n", target)
+
+	var wg sync.WaitGroup
+
+	for port := 1; port <= 1024; port++ {
+		wg.Add(1)
+
+		go func(p int) {
+			defer wg.Done()
+			scanPort(target, p)
+		}(port)
+
+	}
+
+	wg.Wait()
+	fmt.Println("[*] Scan completed.")
+}
+
+func scanPort(ip string, port int) {
+	address := fmt.Sprintf("%s:%d", ip, port)
+
+	conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond)
+
+	if err != nil {
+		return
+	}
+
+	conn.Close()
+	fmt.Printf("[+] PORT FOUND: %d\n", port)
+
 }
